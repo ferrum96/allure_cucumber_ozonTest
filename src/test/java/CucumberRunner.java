25d3c696f04c39fed3d20ee55540f5f71052aff3
Константин <ferrum9696@mail.ru>
2020-04-03 13:55:46 +0300

    Last Task

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = { "src/test/resources/features" },
         glue = {"ru.appline.autotests.steps"},
-        tags = {"@scenario1 , @scenario2"}
+        tags = {"@scenario1"}
 
 )
 public class CucumberRunner {
